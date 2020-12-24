07e6bb6d08129403df9665397b24a0f89ac22901
AlexCrossIT <AlexCrossIT@live.ru>
2020-12-24 21:28:08 +0300

    add import @CrossOrigin

--- src/main/java/com/factory/menufactory/restcontroller/RecipeRestController.java
+++ src/main/java/com/factory/menufactory/restcontroller/RecipeRestController.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.factory.menufactory.other.Response;
 import com.factory.menufactory.service.RecipeService;
