890846434edd25df8dfbca3283d95a8e0e42e47b
AlexCross <AlexCrossIT@live.ru>
2021-01-17 15:13:02 +0300

    added Ingredient repository test

---
+++ src/test/java/com/factory/menufactory/repository/IngredientRepositoryIngtegrationTest.java
@@ -0,0 +1,40 @@
+package com.factory.menufactory.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import com.factory.menufactory.model.Ingredient;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class IngredientRepositoryIngtegrationTest {
+
+	@Autowired
+	private IngredientRepository ingredientRepository;
+	
+	@Test
+	public void shouldCreateAndDeleteNewIngredient() throws Exception{
+	
+		Ingredient ingredient = new Ingredient();
+		ingredient.setIngredientName("CreateAndRemoveNewIngredient");
+		ingredientRepository.save(ingredient);
+		ingredientRepository.flush();
+				
+		Ingredient actualIngredient = ingredientRepository.findById(ingredient.getIngredientId()).get();		
+		
+		assertEquals(ingredient, actualIngredient);
+		
+		ingredientRepository.delete(ingredient);
+		ingredientRepository.flush();
+		
+		assertEquals(false, ingredientRepository.findById(ingredient.getIngredientId()).isPresent());
+		
+	}
+
+	
+}
