3a8170e6b025111d9f13717486de37c703a4782e
AlexCross <AlexCrossIT@live.ru>
2021-06-05 17:35:59 +0300

    Added @CrossOrigin

--- src/main/java/com/factory/menufactory/controller/MenuController.java
+++ src/main/java/com/factory/menufactory/controller/MenuController.java
@@ -3,6 +3,7 @@ package com.factory.menufactory.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import com.factory.menufactory.model.Menu;
 import com.factory.menufactory.service.MenuService;
 
+@CrossOrigin
 @Controller
 public class MenuController {
 
